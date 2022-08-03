1290b95f19d8acc2e61222c6afaec1e549165acb
Leonardo Overbeck da Silva <overbecksilva.leonardo@gmail.com>
2022-08-02 21:35:24 -0300

    adjusting connection string

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -3,6 +3,6 @@ import { dbPassword } from './dbConfig.js';
 
 
 // DO NOT COMMIT
-mongoose.connect(`mongodb+srv://unleaked-moner-managey:${ dbPassword }@moner-managey.zw8enwn.mongodb.net/moner-managey);
+mongoose.connect(`mongodb+srv://unleaked-moner-managey:${ dbPassword }@moner-managey.zw8enwn.mongodb.net/moner-managey`);
 
 export default mongoose.connection;
